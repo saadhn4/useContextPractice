4f0369faf0c59b5fab5f17ff730adf36f5d9a2e9
saadhn4 <saadcfi4@gmail.com>
2025-04-09 03:16:24 +0400

    comments added

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,3 +1,17 @@
+//useContext allows us to share values between multiple components without passing props through each level
+
+// PROVIDER COMPONENT
+// 1. import {createContext} from 'react';
+// 2. export const MyContext = createContext();
+// 3. <MyContext.Provider value={value}>
+//      <Child />
+//    </MyContext.Provider>
+
+// CONSUMER COMPONENTS
+// 1. import React, { useContext } from 'react';
+//    import { MyContext } from './ComponentA';
+// 2. const value = useContext(MyContext);
+
 import React from "react";
 import ComponentA from "./ComponentA";
 
